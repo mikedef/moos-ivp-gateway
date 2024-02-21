7bf4deb6e5c3689ca2cf4c9f17efd9d0595064b7
mikedef <mikedef@mit.edu>
2024-02-21 18:23:56 -0500

    update copyright

--- src/iMOOSGateway/MOOSGateway.cpp
+++ src/iMOOSGateway/MOOSGateway.cpp
@@ -3,7 +3,7 @@
 /*    ORGN: Dept of Mechanical Engineering, MIT, Cambridge MA  */
 /*    FILE: MOOSGateway.h                                      */
 /*    DATE: 2022-11-04                                         */
-/* Copyrght MIT and author/s of software.                      */
+/* Copyright MIT and author/s of software.                     */
 /* This is unreleased BETA code. no permission is granted or   */
 /* implied to use, copy, modify, and distribute this software  */
 /* except by the author(s), or those designated by the author. */
